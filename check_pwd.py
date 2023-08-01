ab03d17ac42ee4d6f0906d5c25cff4331aa8c73b
aaronanderson7 <107898465+aaronanderson7@users.noreply.github.com>
2023-08-01 16:14:46 -0700

    Commit 15: Modified check_pwd to reject passwords that do not contain at least one of the required symbols (~`!@#$%^&*()_+-=), and test_8 passed.

--- check_pwd.py
+++ check_pwd.py
@@ -1,6 +1,8 @@
 # Inserting the Basic Code for the check_pwd function
 
 def check_pwd(pwd):
+    symbols = "~`!@#$%^&*()_+-="
+
     if len(pwd) < 8 or len(pwd) > 20:
         return False
     if not any(character.islower() for character in pwd):
@@ -9,4 +11,7 @@ def check_pwd(pwd):
         return False
     if not any(character.isdigit() for character in pwd):
         return False
+    if not any(character in symbols for character in pwd):
+        return False
+
     return True
